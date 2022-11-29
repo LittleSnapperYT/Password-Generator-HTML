bc55d0055c8a0dbd0bd4a58ef751f76dc1e3ae1d
LittleSnapperYT <75040166+LittleSnapperYT@users.noreply.github.com>
2022-11-28 20:22:10 -0700

    Add files via upload

---
+++ gen.js
@@ -0,0 +1,10 @@
+function generate() {
+    passwordText = document.getElementById("password");
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+    passwordText.innerHTML = password; }
+}
